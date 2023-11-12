8541b75cdb13fd00df5418a7fcf95af58b73d1fe
cutymeoRBLX <hkhanhnam81@gmail.com>
2023-11-12 17:04:35 +0700

    remove dist

--- dist/bot/permissions.d.ts
+++
@@ -1,3 +0,0 @@
-declare function getUserPermissions(user_id: string): string[];
-declare function userHasPermission(user_id: string, permissionList: string[]): boolean;
-export { getUserPermissions, userHasPermission };
--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function createClient(prefix: string, clientId: string, token: string): void;
