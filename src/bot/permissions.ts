abdc06b9a6756a6d193c48beec2f861de5d2bcd2
cyrissisreal <shallowdoesbusiness@gmail.com>
2024-01-07 02:02:39 -0400

    perms fix

--- src/bot/permissions.ts
+++ src/bot/permissions.ts
@@ -2,13 +2,13 @@ const PERMISSIONS_LIST: {
     [permissionName: string]: string[]
 } = {
     SERVERMOD: ["456202569740713986", "1182869627169148999", "460056192509542400"],
-    MODERATOR: ["456202569740713986", "1182869627169148999", "460056192509542400"],
+    INGAMEMOD: ["456202569740713986", "1182869627169148999", "460056192509542400"],
 }
 
 function getUserPermissions(user_id: string): string[] {
     const userPermissions: string[] = [];
     Object.keys(PERMISSIONS_LIST).forEach((permissionName: string) => {
-        if (PERMISSIONS_LIST.MODERATOR.indexOf(user_id) != -1) {
+        if (PERMISSIONS_LIST.INGAMEMOD.indexOf(user_id) != -1) {
             userPermissions.push(permissionName);
         }
     });
