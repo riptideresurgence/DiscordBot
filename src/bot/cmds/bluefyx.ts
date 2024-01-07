b3cc01c7434f9c13a8a36964c735a3cf5b1e8fc6
cyrissisreal <shallowdoesbusiness@gmail.com>
2024-01-06 20:17:58 -0400

    bluefyx

---
+++ src/bot/cmds/bluefyx.ts
@@ -0,0 +1,16 @@
+import { ChatInputCommandInteraction, Message, SlashCommandBuilder, SlashCommandStringOption } from "discord.js";
+import { botCompatibilityLayer } from "../client"
+
+import * as core from "@riptide/core";
+
+module.exports = {
+	slashData: new SlashCommandBuilder()
+		.setName("bluefyx")
+		.setDescription("bluefyx")
+        .addStringOption((option: SlashCommandStringOption) => option.setName("bluefyx").setDescription("bluefyx").setRequired(true)),
+	async execute(interaction: ChatInputCommandInteraction<any> | Message<boolean>, args: any[]) {
+		const newLayer = new botCompatibilityLayer(interaction, true);
+        await newLayer.init(false);
+        return newLayer.reply(`https://media.discordapp.net/attachments/1088196021982609420/1168722340419489844/caption.gif`)
+	},
+};
