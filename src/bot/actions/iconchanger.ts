ff43e41018a98634552e9da21bcab46b25021ac1
cyrissisreal <shallowdoesbusiness@gmail.com>
2024-01-31 18:42:09 -0400

    hoping for the best here

--- src/bot/actions/iconchanger.ts
+++ src/bot/actions/iconchanger.ts
@@ -1,16 +1,15 @@
-// const { Client, Intents } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
-// so true
-import { botClient } from "../client";
-
-//const client = new Client({
-//    intents: [
-//        Intents.FLAGS.GUILDS
-//    ],
-//});
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS
+    ],
+});
 
 const guildId = '1173630297657585725';
-const INTERVAL = 43200000
+const coolInterval = 43200000
+
+let intervalHandler: NodeJS.Timeout | undefined = undefined;
 
 const possibleIcons = [
     "https://media.discordapp.net/attachments/1176997797485101057/1202371817390604369/256px-S3_Weapon_Main_Slosher.png",
@@ -19,63 +18,36 @@ const possibleNames = [
     "sloshermapped."
 ];
 
-let currentClient: botClient | undefined = undefined;
-let intervalHandler: NodeJS.Timeout | undefined = undefined;
+function changeGuildIcon() {
+    try {
+        const guild = await client.guilds.fetch(guildId);
 
+        const coolNumber = Math.floor(Math.random() * possibleIcons.length)
 
-//client.once('ready', () => {
-//    changeGuildIcon();
-//    setInterval(changeGuildIcon, 12 * 60 * 60 * 1000);
-//});
+        if (!guild) {
+            throw new Error('Guild not found.');
+        }
 
-//async function changeGuildIcon() {
-//  try {
-//      const guild = await client.guilds.fetch(guildId);
-
-//      const coolNumber = Math.floor(Math.random() * possibleIcons.length)
-
-//      if (!guild) {
-//          throw new Error('Guild not found.');
-//      }
-//
-//      guild.setIcon(possibleIcons[coolNumber]);
-//      guild.setName(possibleNames[coolNumber]);
-//  } catch (error) {
-//       
-//  }
-//}
-
-function setClient(client: botClient) {
-    currentClient = client;
-}
-
-function setInfo() {
-    if (!currentClient) {
-        return;
+        guild.setIcon(possibleIcons[coolNumber]);
+        guild.setName(possibleNames[coolNumber]);
+    } catch (error) {
+       
     }
-    
-    const guild = currentClient.guilds.fetch(guildId);
-
- 
-    const coolNumber = Math.floor(Math.random() * possibleIcons.length)
-
-   
-    if (!guild) {
-        return;
-    }
-
-    guild.setIcon(possibleIcons[coolNumber]);
-    guild.setName(possibleNames[coolNumber]);
 }
 
 function run() {
-    setInfo()
+    changeGuildIcon()
     if (intervalHandler) {
         clearInterval(intervalHandler);
     }
     intervalHandler = setInterval(() => {
-        setInfo()
-    }, INTERVAL);
+        if (!client) {
+            return;
+        }
+        changeGuildIcon()
+    }, coolInterval);
+
+
 }
 
-export { setClient, run }
+export { run }
