619393792a94e58e1d280adf77fee51760952d5f
cyrissisreal <shallowdoesbusiness@gmail.com>
2024-01-31 18:13:05 -0400

    lets see how this goes lol

--- src/bot/actions/iconchanger.ts
+++ src/bot/actions/iconchanger.ts
@@ -1,13 +1,17 @@
-const { Client, Intents } = require('discord.js');
+// const { Client, Intents } = require('discord.js');
 
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS
-    ],
-});
+// so true
+import { TextChannel } from "discord.js";
+import { botClient } from "../client";
 
-const channelId = '1176997797485101057';
+//const client = new Client({
+//    intents: [
+//        Intents.FLAGS.GUILDS
+//    ],
+//});
+
 const guildId = '1173630297657585725';
+const INTERVAL = 43200000
 
 const possibleIcons = [
     "https://media.discordapp.net/attachments/1176997797485101057/1202371817390604369/256px-S3_Weapon_Main_Slosher.png",
@@ -16,30 +20,63 @@ const possibleNames = [
     "sloshermapped."
 ];
 
-const linkRegexp = new RegExp("\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))");
-function findLinksWithExtension(content: string, extension: string) {
-    const foundLinks = linkRegexp.exec(content);
-    return foundLinks ? foundLinks.filter((link) => link.indexOf(extension) != -1) : [];
+let currentClient: botClient | undefined = undefined;
+let intervalHandler: NodeJS.Timeout | undefined = undefined;
+
+
+//client.once('ready', () => {
+//    changeGuildIcon();
+//    setInterval(changeGuildIcon, 12 * 60 * 60 * 1000);
+//});
+
+//async function changeGuildIcon() {
+//  try {
+//      const guild = await client.guilds.fetch(guildId);
+
+//      const coolNumber = Math.floor(Math.random() * possibleIcons.length)
+
+//      if (!guild) {
+//          throw new Error('Guild not found.');
+//      }
+//
+//      guild.setIcon(possibleIcons[coolNumber]);
+//      guild.setName(possibleNames[coolNumber]);
+//  } catch (error) {
+//       
+//  }
+//}
+
+function setClient(client: botClient) {
+    currentClient = client;
 }
 
-client.once('ready', () => {
-    changeGuildIcon();
-    setInterval(changeGuildIcon, 12 * 60 * 60 * 1000);
-});
-
-async function changeGuildIcon() {
-    try {
-        const guild = await client.guilds.fetch(guildId);
-
-        const coolNumber = Math.floor(Math.random() * possibleIcons.length)
-
-        if (!guild) {
-            throw new Error('Guild not found.');
-        }
-
-        guild.setIcon(possibleIcons[coolNumber]);
-        guild.setName(possibleNames[coolNumber]);
-    } catch (error) {
-       
+function setInfo() {
+    if (!currentClient) {
+        return;
     }
+    
+    const guild = currentClient.guilds.fetch(guildId);
+
+ 
+    const coolNumber = Math.floor(Math.random() * possibleIcons.length)
+
+  
+    if (!guild) {
+        return;
+    }
+
+guild.setIcon(possibleIcons[coolNumber]);
+guild.setName(possibleNames[coolNumber]);
 }
+
+function run() {
+    setInfo()
+    if (intervalHandler) {
+        clearInterval(intervalHandler);
+    }
+    intervalHandler = setInterval(() => {
+        setInfo()
+    }, INTERVAL);
+}
+
+export { setClient, run }
