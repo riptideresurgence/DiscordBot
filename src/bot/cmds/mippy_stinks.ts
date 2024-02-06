5b1d0626695a3bbb5393520e3298cd08d0fc5c46
cyrissisreal <shallowdoesbusiness@gmail.com>
2024-02-06 12:17:19 -0400

    lol

--- src/bot/cmds/mippy_stinks.ts
+++ src/bot/cmds/mippy_stinks.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, Message, SlashCommandBuilder } from "discord.js";
+import { TextChannel, ChatInputCommandInteraction, Message, SlashCommandBuilder } from "discord.js";
 import { botClient, botCompatibilityLayer } from "../client"
 
 let currentClient: botClient | undefined = undefined;
@@ -12,12 +12,26 @@ module.exports = {
 		.setName("mippy_stinks")
 		.setDescription("Pings mippy 10 times."),
 	async execute(interaction: ChatInputCommandInteraction<any> | Message<boolean>, args: any[]) {
+		if (!currentClient) {
+            return;
+        }
+
 		const newLayer = new botCompatibilityLayer(interaction, true);
         await newLayer.init(false);
 
 		let foundChannel = currentClient.channels.cache.get(`1173630298936856708`);
         if (foundChannel) {
             (foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
+			(foundChannel as TextChannel).send(`<@876953124546420830>`);
         }
 	},
 };
