6207ba613dac3aec76691de6c078ac8c1acf2a87
cyrissisreal <shallowdoesbusiness@gmail.com>
2024-02-10 21:57:47 -0400

    man

--- src/bot/cmds/view_post.ts
+++ src/bot/cmds/view_post.ts
@@ -32,7 +32,7 @@ module.exports = {
         
         core.roblox.getEntryFromDataStore(5113672776, "PostDataStore", "Posts")
             .then((entry) => {
-                const postTable = entry.Posts;
+                const postTable = entry;
                 if (!postTable) {
                     return newLayer.reply("Couldn't fetch post data.");
                 }
